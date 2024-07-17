56fbacaa8094882baf5ef9f16bbbf107db4c893a
yinghaidada <pyj2897022134@gmail.com>
2024-07-17 15:24:36 +0800

    删除多余lua

--- NodeEditor/main/main.cpp
+++ NodeEditor/main/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <core/nepch.h>
+
+struct DIY {
+    int a;
+    int b;
+    char *name;
+
+    std::ostream& operator<<(std::ostream& os, const DIY& dt) {
+        os << dt.a << "/" << dt.b << "/" << dt.name;
+        return os;
+    }
+};
 
 int main(int argc, char** argv){
 
     std::cout << "Hello World!" << std::endl;
+    NodeEditor::Ref<DIY> d1;
+    d1 = NodeEditor::CreateRef<DIY>();
+
+    std::cout << d1 << std::endl;
+
     return 0;
 }
