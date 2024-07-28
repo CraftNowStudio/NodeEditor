ecb074b4bb145bcead94b89c28cf6b3dfbe15e86
yinghaidada <pyj2897022134@gmail.com>
2024-07-28 09:29:48 +0800

    更新格式化设置，禁用include排序

--- NodeEditor/main/main.cpp
+++ NodeEditor/main/main.cpp
@@ -3,8 +3,8 @@
 //#include "imgui_sdl2_vulkan.h"
 //#include "imgui_glfw_vulkan.h"
 //#include "imgui_sdl2_opengl3.h"
-#include "imgui_glfw_opengl3.h"
 #include <core/Application.h>
+#include "imgui_glfw_opengl3.h"
 
 class Sandbox : public NodeEditor::Application {
 public:
